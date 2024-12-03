996c840eb1a410bc0dfbbb8d700b80e0f2898244
Ryan Teodoro <thechori@gmail.com>
2024-12-03 01:41:30 -0600

    generate base resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ExercisesModule } from './exercises/exercises.module';
+import { WorkoutsModule } from './workouts/workouts.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ExercisesModule, WorkoutsModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
